51192324f7c6d6d904d4c5908280955a907010b6
pallen7 <paulallen7@msn.com>
2015-10-12 19:24:17 +0100

    More Haskell

--- Learn you a Haskell/5 Higher Order Functions/2 Lambdas Folds and More HOF.hs
+++ Learn you a Haskell/5 Higher Order Functions/2 Lambdas Folds and More HOF.hs
@@ -19,3 +19,50 @@ addOneWithLambda = map (\x -> x+1) [1..5]
 
 -- Comes in a lot more handy when you want to carry out a few operations on a given value without defining a function
 doALotOfStuff = map (\x -> x^2 * 3) [1..10]
+
+
+-- ** FOLDS::
+-- Fold a list from left to right with foldl (or right to left with foldr) whilst applying a function to an accumulator
+-- With foldl the accumulator is the first parameter of the function and we work through the list from l->r
+-- also needs to take in a seed
+mySum :: [Int] -> Int
+mySum xs = foldl(\acc x -> acc + x) 0 xs
+
+-- Due to currying we can also do this:
+simpleSum :: [Int] -> Int
+simpleSum = foldl (+) 0
+
+useMySum = mySum [1,2,3,4,5]
+useMySimpleSum = simpleSum [1,2,3,4,5]
+
+
+-- Difference between foldl & foldr
+minusLeftFold :: [Int] -> Int
+minusLeftFold xs = foldl(\acc x -> acc - x) 0 xs
+
+minusRightFold :: [Int] -> Int
+minusRightFold xs = foldr(\x acc -> x - acc) 0 xs
+
+useMinusLeftFold = minusLeftFold [2,3,1]
+useMinusRightFold = minusRightFold [2,3,1]
+
+
+-- Also the type of the list we are folding and the return value don't have to be the same:
+myElem :: Eq a => [a] -> a -> Bool
+myElem xs y = foldl (\acc x -> if x == y then True else acc) False xs
+
+useMyElem = myElem [1,2,3,4,5] 4
+
+
+-- foldl1 and foldr1 are the same as the other folds but you don't need to supply a starting value
+myOtherSum :: [Int] -> Int
+myOtherSum xs = foldl1(\acc x -> acc + x) xs
+
+useMyOtherSum = myOtherSum [1,2,3,4,5]
+
+
+
+
+
+
+
---
+++ Project Euler/Project Euler 9.hs
@@ -0,0 +1,29 @@
+-- A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+-- a2 + b2 = c2
+-- For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+-- so 3+4+5 = 12
+
+-- There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+-- Find the product abc.
+
+-- So we only know our starting number and that there is exactly one set
+
+-- 3^2 = 9
+-- 4^2 = 16
+-- 5^2 = 25
+-- 6^2 = 36
+-- 7^2 = 49
+-- 8^2 = 64
+-- 9^2 = 81
+-- 10^2 = 100
+
+-- hmmmmm...
+-- 3^2 + 4^2 = 5^2
+-- So 6^2 + 8^2 = 10^2.... How convenient!!!  6 + 8 + 10 = 14
+
+-- 2 more to test:
+
+-- 9^2 + 12^2 = 15^2
+-- 12^2 + 16^2 = 20^2
+
