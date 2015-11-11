f193a7824853b3b428b7fb5005d2d04513a3c03e
pallen7 <paulallen7@msn.com>
2015-11-11 00:15:22 +0000

    FP101x - Higher Order Functions

--- FP101x/5 Higher Order Functions/exercises.hs
+++ FP101x/5 Higher Order Functions/exercises.hs
@@ -10,9 +10,15 @@ all' :: (a -> Bool) -> [a] -> Bool
 all' f xs = and [f x | x <- xs]
 useall = all' even [2,4,6,9]
 
+all2 :: (a -> Bool) -> [a] -> Bool
+all2 p xs = foldl (&&) True (map p xs)
+
 any' :: (a -> Bool) -> [a] -> Bool
 any' f xs = or [f x | x <- xs]
 useany = any' odd [2,4,6,9]
+
+any2 :: (a -> Bool) -> [a] -> Bool
+any2 p xs = foldr (||) True (map p xs)
 
 takeWhile' :: (a -> Bool) -> [a] -> [a]
 takeWhile' _ [] = []
@@ -45,7 +51,22 @@ filter' f = foldr(\x xs -> if f x then x : xs else xs) []
 dec2int :: [Int] -> Int
 dec2int = foldl(\acc n -> acc*10 + n) 0
 
-filter2' :: (a -> Bool) -> [a] -> [a]
-filter2' f = foldl(\x xs -> if f x then x : xs else xs) []
+{-
+5 why is sumsqreven = compose [sum, map (^2), filter even] invalid?
+-- because sum maps from [a] -> a
+-}
+compose :: [a -> a] -> (a -> a)
+compose = foldr (.) id
 
+{-
+6 curry: convert a function that takes a pair into a function that takes it's arguments one at a time
+-}
+curry' :: ((a,b) -> c) -> a -> b -> c
+curry' f = \x y -> f (x,y)
+
+{-
+6 uncurry
+-}
+uncurry' :: (a -> b -> c) -> (a, b) -> c
+uncurry' f = \(x,y) -> f x y
 
